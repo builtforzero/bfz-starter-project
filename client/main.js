923b7fcab329924a740bf7d0f15a771dd2c1ca9f
koffeeya <kavya.beheraj@gmail.com>
2020-09-23 16:24:00 -0400

    updated readme to use yarn

--- client/main.js
+++ client/main.js
@@ -1,4 +1,7 @@
-/* const d3 = require('d3')
-const AWS = require('aws-sdk');
-const Papa = require('papaparse');
-require('dotenv').config(); */
+const d3 = require('d3');
+/* Add any other packages */
+require('dotenv').config();
+
+/* Pull in secrets from the .env file, if needed */
+const apiKey = process.env.API_KEY;
+const anotherSecret = process.env.ANOTHER_SECRET;
